961ca0d21e1dc05a8233c60191bd17469b4bb36e
iamnikitakostin <141674557+iamnikitakostin@users.noreply.github.com>
2024-01-03 10:51:12 -0800

    fixed styling and added unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+
+test("Time is there & can reserve", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const link = screen.getByText("Reservations");
+  fireEvent.click(link);
+
+  const time = screen.getByLabelText("Time");
+  fireEvent.click(time)
+
+  const agree = screen.getByLabelText("I accept the booking policy.");
+  fireEvent.click(agree)
+
+  const continueBtn = screen.getByText("Make Your reservation");
+  fireEvent.click(continueBtn)
+
+  const logo = screen.getByAltText("logo");
+
+  expect(logo).toBe;
 });
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,13 @@ import React from 'react'
 import logo from "../../assets/aa38384d3942a55696d8070552aed2f4c190fc14.jpg"
 import Nav from "../Nav/Nav"
 import "./Header.css"
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header className='app__header'>
       <div className='app__header-logo'>
-        <img src={logo} alt='logo' />
+        <Link to='/'><img src={logo} alt='logo' /></Link>
       </div>
       <Nav/>
     </header>
